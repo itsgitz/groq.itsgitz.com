a9bf4a810f6d0cb5d76654b8961ffe1ebac20f67
itsgitz <anggit.ginanjar.dev@gmail.com>
2024-10-07 04:52:33 +0700

    feat: added groq result

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,55 @@
-export default function Home() {
+import Groq from "groq-sdk";
+
+async function groqApi() {
+  'use server'
+
+  if (!process.env.GROQ_API_KEY) {
+    return undefined
+  }
+
+  const groq = new Groq({ apiKey: process.env.GROQ_API_KEY })
+
+  const completion = groq.chat.completions.create({
+    messages: [
+      {
+        role: 'user',
+        content: 'Explain Indonesia dalam bahasa Indonesia'
+      }
+    ],
+    model: 'llama3-8b-8192'
+  })
+
+  if (!completion) {
+    return undefined
+  }
+
+  return completion
+}
+
+export default async function Home() {
+  const chatCompletion = await groqApi()
+  const result = chatCompletion?.choices[0].message.content
+
+  if (!result) {
+    return (
+      <div className="container mx-auto">
+        <div className="py-3">
+          <h1 className="text-4xl">groq!</h1>
+        </div>
+        <div className="py-3">
+          unable to proceed your request right now 😔
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className="container mx-auto"> 
       <div className="py-3">
-        <h1 className="text-4xl">grok!</h1>
+        <h1 className="text-4xl">groq!</h1>
+      </div>
+      <div className="py-3">
+        {result}
       </div>
     </div> 
   );
