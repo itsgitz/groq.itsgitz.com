794e8b571492a79491c94a42cca8035a76633084
itsgitz <anggit.ginanjar.dev@gmail.com>
2024-10-09 23:23:58 +0700

    feat: ask question

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,100 @@
-import Groq from "groq-sdk";
+'use client'
 
-async function groqApi() {
-  'use server'
+import {Button} from "@/components/ui/button";
+import {Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
+import {Textarea} from "@/components/ui/textarea";
+import {useToast} from "@/hooks/use-toast";
+import {groqApi} from "@/lib/groq";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {useState} from "react";
+import {useForm} from "react-hook-form";
+import { z } from 'zod'
 
-  if (!process.env.GROQ_API_KEY) {
-    return undefined
-  }
+const FormSchema = z.object({
+  question: z
+    .string()
+    .min(3, {
+      message: "Question must be at least 3 characters.",
+    }) 
+})
 
-  const groq = new Groq({ apiKey: process.env.GROQ_API_KEY })
+type Form = z.infer<typeof FormSchema>
 
-  const completion = groq.chat.completions.create({
-    messages: [
-      {
-        role: 'user',
-        content: 'Explain Indonesia dalam bahasa Indonesia'
-      }
-    ],
-    model: 'llama3-8b-8192'
+export default function Home() { 
+  const { toast } = useToast()
+  const [answer, setAnswer] = useState('')
+  const [loading, setLoading] = useState(false)
+  const form = useForm<Form>({
+    resolver: zodResolver(FormSchema),
   })
 
-  if (!completion) {
-    return undefined
-  }
+  async function onSubmit(data: Form) {
+    const question = data.question
+    setLoading(true)
 
-  return completion
-}
+    console.log('my question is', question)
 
-export default async function Home() {
-  const chatCompletion = await groqApi()
-  const result = chatCompletion?.choices[0].message.content
+    const result = await groqApi(question)
 
-  if (!result) {
-    return (
-      <div className="container mx-auto">
-        <div className="py-3">
-          <h1 className="text-4xl">groq!</h1>
-        </div>
-        <div className="py-3">
-          unable to proceed your request right now 😔
-        </div>
-      </div>
-    )
-  }
+    const answer = result?.choices[0].message.content
+    if (!answer) {
+      setAnswer(`Sorry, I couldn't understand your question 😔`)
+    }
+    setAnswer(answer!)
+
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    })
+
+
+    setLoading(false)
+    form.reset({ 'question': '' })
+  } 
 
   return (
     <div className="container mx-auto"> 
       <div className="py-3">
-        <h1 className="text-4xl">groq!</h1>
+        <h1 className="text-4xl">Ask Gitz AI - Powered by <a href="https://groq.com/" target="_blank">Groq</a></h1>
       </div>
+      <div className="py-3"> 
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+            <FormField
+              control={form.control}
+              name="question"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Apa yang ingin kau tanyakan?</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      placeholder="Tanya aku apa saja ..."
+                      className="resize-none"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    Aku akan berusaha sebisa mungkin untuk menjawabmu dengan Bahasa Indonesia
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </div>
+
       <div className="py-3">
-        {result}
+        {
+          (loading === true)
+            ? 'Sebentar, aku sedang mikir ...'
+            : answer
+        }
       </div>
     </div> 
   );
---
+++ src/lib/groq.ts
@@ -0,0 +1,28 @@
+'use server'
+
+import Groq from "groq-sdk";
+
+export async function groqApi(question: string) {
+
+  if (!process.env.GROQ_API_KEY) {
+    return undefined
+  }
+
+  const groq = new Groq({ apiKey: process.env.GROQ_API_KEY })
+
+  const completion = groq.chat.completions.create({
+    messages: [
+      {
+        role: 'user',
+        content: `${question} in Indonesian`
+      }
+    ],
+    model: 'llama3-8b-8192'
+  })
+
+  if (!completion) {
+    return undefined
+  }
+
+  return completion
+}
